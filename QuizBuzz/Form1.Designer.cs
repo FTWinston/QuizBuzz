13abc52a13966478b2fa05ea05d0fe64fb28a2e5
FTWinston <ftwinston@gmail.com>
2016-04-19 17:53:25 +0100

    Displaying a list of connections

--- QuizBuzz/Form1.Designer.cs
+++ QuizBuzz/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lblAddress = new System.Windows.Forms.Label();
+            this.gridBuzzers = new System.Windows.Forms.DataGridView();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSound = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridBuzzers)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -51,17 +55,54 @@
             this.lblAddress.TabIndex = 0;
             this.lblAddress.Text = "blah";
             // 
+            // gridBuzzers
+            // 
+            this.gridBuzzers.AllowUserToAddRows = false;
+            this.gridBuzzers.AllowUserToDeleteRows = false;
+            this.gridBuzzers.AllowUserToResizeRows = false;
+            this.gridBuzzers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridBuzzers.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gridBuzzers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridBuzzers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colName,
+            this.colSound});
+            this.gridBuzzers.Location = new System.Drawing.Point(16, 54);
+            this.gridBuzzers.Name = "gridBuzzers";
+            this.gridBuzzers.RowHeadersVisible = false;
+            this.gridBuzzers.RowTemplate.Height = 28;
+            this.gridBuzzers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.gridBuzzers.Size = new System.Drawing.Size(726, 348);
+            this.gridBuzzers.TabIndex = 1;
+            // 
+            // colName
+            // 
+            this.colName.DataPropertyName = "Name";
+            this.colName.HeaderText = "Buzzer Name";
+            this.colName.Name = "colName";
+            // 
+            // colSound
+            // 
+            this.colSound.DataPropertyName = "SoundName";
+            this.colSound.HeaderText = "Buzzer Sound";
+            this.colSound.Name = "colSound";
+            this.colSound.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colSound.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(754, 414);
+            this.Controls.Add(this.gridBuzzers);
             this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "QuizBuzz";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridBuzzers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +112,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.DataGridView gridBuzzers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn colSound;
     }
 }
 
--- QuizBuzz/Form1.cs
+++ QuizBuzz/Form1.cs
@@ -17,12 +17,24 @@ namespace QuizBuzz
         }
 
         WebServer server;
+        SortedList<int, ConnectionInfo> connections;
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            connections = new SortedList<int, ConnectionInfo>();
             InitializeServer();
             DisplayUrl();
             server.Start();
+            colSound.DataSource = GetSounds();
+            canBuzzIn = true;
+
+            connections.Add(1, new ConnectionInfo() { Name = "blah" });
+            connections.Add(2, new ConnectionInfo() { Name = "blah2" });
+
+            gridBuzzers.AutoGenerateColumns = false;
+            var bindingSource = new BindingSource();
+            bindingSource.DataSource = connections.Values;
+            gridBuzzers.DataSource = bindingSource;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -42,14 +54,68 @@ namespace QuizBuzz
              && !string.IsNullOrEmpty(server.SetOption("listening_port", "8080")))
                 server.SetOption("listening_port", "0"); // auto-allocate a port
 
-            server.WebSocketConnected += (i) => Console.WriteLine("{0} Connected", i);
-            server.WebSocketDisconnected += (i) => Console.WriteLine("{0} Disconnected", i);
-            server.WebSocketMessageReceived += (i, m) => Console.WriteLine("{0} sent: {1}", i, m);
+            server.WebSocketConnected += server_WebSocketConnected;
+            server.WebSocketDisconnected += server_WebSocketDisconnected;
+            server.WebSocketMessageReceived += server_WebSocketMessageReceived;
+        }
+
+        void server_WebSocketConnected(int id)
+        {
+            Console.WriteLine("{0} Connected", id);
+            connections.Add(id, new ConnectionInfo());
+        }
+
+        void server_WebSocketDisconnected(int id)
+        {
+            Console.WriteLine("{0} Disconnected", id);
+            connections.Remove(id);
+        }
+
+        bool canBuzzIn;
+        void server_WebSocketMessageReceived(int id, string message)
+        {
+            Console.WriteLine("{0} sent: {1}", id, message);
+
+            if (message.StartsWith("name "))
+            {
+                connections[id].Name = message.Substring(5).Trim();
+            }
+            else if (message == "buzz" && canBuzzIn)
+            {
+                canBuzzIn = false;
+                foreach (var conn in connections)
+                    server.SendWebSocketMessage(conn.Key, conn.Key == id ? "win" : "lose");
+
+                var timer = new Timer();
+                timer.Interval = 5000; // TODO: add a "quick" mode or other way of having this reduced, for introductory purposes. Also have a "manual" mode, with a button to re-allow.
+                timer.Tick += (o, e) =>
+                {
+                    AllowBuzzIn();
+                    timer.Stop();
+                };
+                timer.Start();
+            }
+        }
+
+        private void AllowBuzzIn()
+        {
+            foreach (var conn in connections)
+                server.SendWebSocketMessage(conn.Key, "ready");
+            canBuzzIn = true;
         }
 
         private void DisplayUrl()
         {
             lblAddress.Text = server.GetUrl();
+        }
+
+        private IList<string> GetSounds()
+        {
+            var sounds = new List<string>();
+            sounds.Add("Sound 1");
+            sounds.Add("Sound 2");
+            sounds.Add("Sound 3");
+            return sounds;
         }
     }
 }
